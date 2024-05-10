5a2c744e507268a50a52dc9c4eafd2ca1305b57d
luyse santos <luysegfsantos@gmail.com>
2024-05-10 19:29:00 -0300

    aula 2

--- semana 6/python/Sabor_Express.py
+++ semana 6/python/Sabor_Express.py
@@ -1,15 +1,40 @@
-print("""
+import os
+# importando uma biblioteca
+def exibir_nome_do_programa():
+    print("""
 █▀ ▄▀█ █▄▄ █▀█ █▀█   █▀▀ █▀ █▀█ █▀█ █▀▀ █▀ █▀ █▀█
 ▄█ █▀█ █▄█ █▄█ █▀▄   ██▄ ▄█ █▀▀ █▀▄ ██▄ ▄█ ▄█ █▄█ \n""")
 # no python pode usar tanto aspas simples quanto duplas
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-# armazena em uma variavel 
-# input para interação
-# f{} serve para interpolação
+def exibir_opcoes():
+    
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurantes')
+    print('3. Ativar restaurantes')
+    print('4. Sair\n') 
 
-opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+def escolher_opcao():
+
+    opcao_escolhida = int(input('Escolha uma opção: '))
+    # opcao_escolhida = int(opcao_escolhida)
+    # int para tranformar string em número
+    
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurantes')
+    else:
+        finalizar_app()
+# condicional
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
+# função main(principal)
+
+if __name__ == '__main__':
+    main()
+# condicional main
