87f70e84386b5120693a53a6669edcddce7c7a4b
luyse santos <luysegfsantos@gmail.com>
2024-05-16 11:56:07 -0300

    aula 3

--- semana 7/oo-sabor-express/restaurante.py
+++ semana 7/oo-sabor-express/restaurante.py
@@ -2,22 +2,28 @@ class Restaurante:
     restaurantes = []
 
     def __init__(self, nome, categoria):
-        self.nome = nome
-        self.categoria = categoria
-        self.ativo = False
+        self.nome = nome.title() #iniciais com letra maiúscula
+        self.categoria = categoria.upper()# todas as letras maiúscula
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 # criando o próprio metododo
     
-
     def __str__(self):
       return f'{self.nome} | {self.categoria}'
 # o metodo __str__ mostra o objeto em formato de texto
 
-    def listar_restaurantes():
-         for restaurante in Restaurante.restaurantes:
-             print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
-restaurante_praca = Restaurante('praça' , 'Gourmet')
+    @property
+    def ativo(self):
+        return '⌧' if self._ativo else '☐'
+# modifica como o atributo vai ser lido.
+
+restaurante_praca = Restaurante('praça' , 'Gourmet')                    
 restaurante_pizza = Restaurante('pizza', 'italiana')
 
 Restaurante.listar_restaurantes()
