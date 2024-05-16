f827ed2035c40ae92f5e2f012c03b0d16ff926b7
luyse santos <luysegfsantos@gmail.com>
2024-05-16 10:29:45 -0300

    primeiro commit

---
+++ semana 7/oo-sabor-express/restaurante.py
@@ -0,0 +1,16 @@
+class Restaurante:
+    nome = ''
+    categoria = ''
+    ativo = False
+
+restaurante_praca = Restaurante()
+restaurante_praca.nome = 'Praça'
+restaurante_praca.categoria = 'Gourmet'
+
+restaurante_pizza = Restaurante()
+
+restaurantes = [restaurante_praca, restaurante_pizza]
+
+print(restaurante_praca)
+# a função dir() retorna os atributos do objeto
+# vars(), mostra o dicionário desses atributos e os métodos
