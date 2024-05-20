69f7ae3548c22669fb8c03b7972851fba6f20e97
luyse santos <luysegfsantos@gmail.com>
2024-05-20 17:38:57 -0300

    curso 2, aula 2

--- semana 7/oo-sabor-express/modelos/cardapio/bebida.py
+++ semana 7/oo-sabor-express/modelos/cardapio/bebida.py
@@ -7,4 +7,7 @@ class Bebida(ItemCardapio):
         self.tamanho = tamanho
 
     def __str__(self):
-        return self._nome    
+        return self._nome   
+     
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.05)
--- semana 7/oo-sabor-express/modelos/cardapio/prato.py
+++ semana 7/oo-sabor-express/modelos/cardapio/prato.py
@@ -4,3 +4,5 @@ class Prato(ItemCardapio):
     def __init__(self, nome, preco, descricao):
         super().__init__ (nome, preco)
         self.descricao = descricao
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.05)
